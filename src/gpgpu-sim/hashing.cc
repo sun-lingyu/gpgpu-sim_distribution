b58ec853b92975e5e1ae3b8b0b140d4d3f34e344
sun-lingyu <1758861056@qq.com>
2024-11-13 19:13:09 +0800

    add IPOLY support for 8, 128 and 256 sets

--- src/gpgpu-sim/hashing.cc
+++ src/gpgpu-sim/hashing.cc
@@ -18,11 +18,14 @@ unsigned ipoly_hash_function(new_addr_type higher_bits, unsigned index,
    * "Sacat: streaming-aware conflict-avoiding thrashing-resistant gpgpu
    * cache management scheme." Khairy et al. IEEE TPDS 2017.
    *
-   * equations for 16 banks are corresponding to IPOLY(5)
+   * equations for 16 banks are corresponding to IPOLY(19)
    * equations for 32 banks are corresponding to IPOLY(37)
    * equations for 64 banks are corresponding to IPOLY(67)
+   * equations for 128 banks are corresponding to IPOLY(131)
+   * equations for 256 banks are corresponding to IPOLY(283)
    * To see all the IPOLY equations for all the degrees, see
    * http://wireless-systems.ece.gatech.edu/6604/handouts/Peterson's%20Table.pdf
+   * or https://www.ece.unb.ca/tervo/ee4253/polyprime.shtml
    *
    * We generate these equations using GF(2) arithmetic:
    * http://www.ee.unb.ca/cgi-bin/tervo/calc.pl?num=&den=&f=d&e=1&m=1
@@ -35,7 +38,18 @@ unsigned ipoly_hash_function(new_addr_type higher_bits, unsigned index,
    * exit in GPGPU applications and also show good performance for other
    * strides.
    */
-  if (bank_set_num == 16) {
+  if (bank_set_num == 8) {
+    std::bitset<64> a(higher_bits);
+    std::bitset<3> b(index);
+    std::bitset<3> new_index(index);
+
+    new_index[0] = a[11] ^ a[10] ^ a[9] ^ a[7] ^ a[4] ^ a[3] ^ a[2] ^ a[0] ^ b[0];
+    new_index[1] = a[12] ^ a[9] ^ a[8] ^ a[7] ^ a[5] ^ a[2] ^ a[1] ^ a[0] ^ b[1];
+    new_index[2] = a[13] ^ a[10] ^ a[9] ^ a[8] ^ a[6] ^ a[3] ^ a[2] ^ a[1] ^ b[2];
+
+    return new_index.to_ulong();
+
+  } else if (bank_set_num == 16) {
     std::bitset<64> a(higher_bits);
     std::bitset<4> b(index);
     std::bitset<4> new_index(index);
@@ -84,11 +98,40 @@ unsigned ipoly_hash_function(new_addr_type higher_bits, unsigned index,
     new_index[5] =
         a[17] ^ a[16] ^ a[15] ^ a[14] ^ a[11] ^ a[9] ^ a[5] ^ a[4] ^ b[5];
     return new_index.to_ulong();
+
+  } else if (bank_set_num == 128) {
+    std::bitset<64> a(higher_bits);
+    std::bitset<7> b(index);
+    std::bitset<7> new_index(index);
+
+    new_index[0] = a[21] ^ a[20] ^ a[19] ^ a[18] ^ a[14] ^ a[12] ^ a[7] ^ a[6] ^ a[0] ^ b[0];
+    new_index[1] = a[22] ^ a[18] ^ a[15] ^ a[14] ^ a[13] ^ a[12] ^ a[8] ^ a[6] ^ a[1] ^ a[0] ^ b[1];
+    new_index[2] = a[19] ^ a[16] ^ a[15] ^ a[14] ^ a[13] ^ a[9] ^ a[7] ^ a[2] ^ a[1] ^ b[2];
+    new_index[3] = a[20] ^ a[17] ^ a[16] ^ a[15] ^ a[14] ^ a[10] ^ a[8] ^ a[3] ^ a[2] ^ b[3];
+    new_index[4] = a[21] ^ a[18] ^ a[17] ^ a[16] ^ a[15] ^ a[11] ^ a[9] ^ a[4] ^ a[3] ^ b[4];
+    new_index[5] = a[22] ^ a[19] ^ a[18] ^ a[17] ^ a[16] ^ a[12] ^ a[10] ^ a[5] ^ a[4] ^ b[5];
+    new_index[6] = a[20] ^ a[19] ^ a[18] ^ a[17] ^ a[13] ^ a[11] ^ a[6] ^ a[5] ^ b[6];
+    return new_index.to_ulong();
+
+  } else if (bank_set_num == 256) {
+    std::bitset<64> a(higher_bits);
+    std::bitset<8> b(index);
+    std::bitset<8> new_index(index);
+
+    new_index[0] = a[21] ^ a[20] ^ a[19] ^ a[17] ^ a[16] ^ a[13] ^ a[12] ^ a[10] ^ a[7] ^ a[5] ^ a[4] ^ a[0] ^ b[0];
+    new_index[1] = a[19] ^ a[18] ^ a[16] ^ a[14] ^ a[12] ^ a[11] ^ a[10] ^ a[8] ^ a[7] ^ a[6] ^ a[4] ^ a[1] ^ a[0] ^ b[1];
+    new_index[2] = a[20] ^ a[19] ^ a[17] ^ a[15] ^ a[13] ^ a[12] ^ a[11] ^ a[9] ^ a[8] ^ a[7] ^ a[5] ^ a[2] ^ a[1] ^ b[2];
+    new_index[3] = a[19] ^ a[18] ^ a[17] ^ a[14] ^ a[9] ^ a[8] ^ a[7] ^ a[6] ^ a[5] ^ a[4] ^ a[3] ^ a[2] ^ a[0] ^ b[3];
+    new_index[4] = a[21] ^ a[18] ^ a[17] ^ a[16] ^ a[15] ^ a[13] ^ a[12] ^ a[9] ^ a[8] ^ a[6] ^ a[3] ^ a[1] ^ a[0] ^ b[4];
+    new_index[5] = a[19] ^ a[18] ^ a[17] ^ a[16] ^ a[14] ^ a[13] ^ a[10] ^ a[9] ^ a[7] ^ a[4] ^ a[2] ^ a[1] ^ b[5];
+    new_index[6] = a[20] ^ a[19] ^ a[18] ^ a[17] ^ a[15] ^ a[14] ^ a[11] ^ a[10] ^ a[8] ^ a[5] ^ a[3] ^ a[2] ^ b[6];
+    new_index[7] = a[21] ^ a[20] ^ a[19] ^ a[18] ^ a[16] ^ a[15] ^ a[12] ^ a[11] ^ a[9] ^ a[6] ^ a[4] ^ a[3] ^ b[7];
+    return new_index.to_ulong();
   } else { /* Else incorrect number of channels for the hashing function */
     assert(
         "\nmemory_partition_indexing error: The number of "
         "channels should be "
-        "16, 32 or 64 for the hashing IPOLY index function. other banks "
+        "8, 16, 32, 64, 128 or 256 for the hashing IPOLY index function. other banks "
         "numbers are not supported. Generate it by yourself! \n" &&
         0);
 
